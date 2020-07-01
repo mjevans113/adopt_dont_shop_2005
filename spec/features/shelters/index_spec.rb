c104e66895893d4a63ef0faad57bb68627629d6a
Michael Evans <michaeljevans113@gmail.com>
2020-07-01 17:02:09 -0600

    Update test to include more shelter info

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'Shelters Index Page' do
-  it 'Displays all shelters to the user' do
-    shelter_1 = Shelter.create(name: 'Pets, Pets, Pets')
-    shelter_2 = Shelter.create(name: 'Gotta Lotta Pets')
+RSpec.describe 'Shelters index page' do
+  it 'displays all shelters to the user' do
+    shelter_1 = Shelter.create(name: 'Pets, Pets, Pets', address: '123 Easy St', city: 'Denver', state: 'CO', zip: '80204')
+    shelter_2 = Shelter.create(name: 'Got A Lotta Pets', address: '456 EzPz Ln', city: 'LVegas', state: 'NV', zip: '12345')
 
     visit '/shelters'
 
     expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content("Address: #{shelter_1.address} City: #{shelter_1.city} State: #{shelter_1.state} ZIP: #{shelter_1.zip}")
     expect(page).to have_content(shelter_2.name)
+    expect(page).to have_content("Address: #{shelter_2.address} City: #{shelter_2.city} State: #{shelter_2.state} ZIP: #{shelter_2.zip}")
   end
 end
