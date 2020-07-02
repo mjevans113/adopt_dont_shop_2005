627f504e1a6c1c375a37ce9e34c1f7df48d40afa
Michael Evans <michaeljevans113@gmail.com>
2020-07-01 20:13:38 -0600

    Add routes for shelter creation and posting to shelters index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
-  
-  get '/shelters', to: 'shelters#index'
-  get '/shelters/:id', to: 'shelters#show'
+
+  get  '/shelters',     to: 'shelters#index'
+  get  '/shelters/new', to: 'shelters#new'
+  post '/shelters',     to: 'shelters#create'
+  get  '/shelters/:id', to: 'shelters#show'
 end
