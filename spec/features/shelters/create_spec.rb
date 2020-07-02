d4287da4c147c7134425cc153e97bd0ec635f016
Michael Evans <michaeljevans113@gmail.com>
2020-07-01 20:12:32 -0600

    Create test for shelter creation

---
+++ spec/features/shelters/create_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Shelters index page' do
+  it 'allows a user to follow the link to create a new shelter' do
+    visit '/shelters'
+
+    expect(page).to_not have_content('All Your Pets Are Belong to Us')
+
+    click_on 'Create New Shelter'
+
+    expect(current_path).to eq('/shelters/new')
+
+    fill_in :name, with: 'All Your Pets Are Belong to Us'
+    fill_in :address, with: '789 Downing St'
+    fill_in :city, with: 'Denver'
+    fill_in :state, with: 'CO'
+    fill_in :zip, with: '80204'
+
+    click_on 'Create Shelter'
+
+    expect(current_path).to eq('/shelters')
+    expect(page).to have_content('All Your Pets Are Belong to Us')
+  end
+end
