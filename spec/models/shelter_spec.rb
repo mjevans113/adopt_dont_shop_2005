d82f922d7cdea4a4de31daff39a016d8f9ae8850
Michael Evans <michaeljevans113@gmail.com>
2020-07-02 19:13:46 -0600

    Add has_many test

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe Shelter do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+  describe 'relationships' do
+    it { should have_many :pets }
+  end
 end
