a5bd129f3e8a083eadf5ee0ca2c13164d376f7ad
Michael Evans <michaeljevans113@gmail.com>
2020-07-03 10:16:15 -0600

    Add route for pet index for a particular shelter

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'welcome#index'
+  get '/', to: 'welcome#index'
 
-  get '/shelters', to: 'shelters#index'
-  get '/shelters/new', to: 'shelters#new'
-  post '/shelters', to: 'shelters#create'
-  get '/shelters/:id/edit', to: 'shelters#edit'
-  patch '/shelters/:id', to: 'shelters#update'
-  delete '/shelters/:id', to: 'shelters#destroy'
-  get '/shelters/:id', to: 'shelters#show'
+  get    '/shelters',          to: 'shelters#index'
+  get    '/shelters/new',      to: 'shelters#new'
+  post   '/shelters',          to: 'shelters#create'
+  get    '/shelters/:id/pets', to: 'shelters#pets'
+  get    '/shelters/:id/edit', to: 'shelters#edit'
+  get    '/shelters/:id',      to: 'shelters#show'
+  patch  '/shelters/:id',      to: 'shelters#update'
+  delete '/shelters/:id',      to: 'shelters#destroy'
 
-  get '/pets', to: 'pets#index'
+  get    '/pets', to: 'pets#index'
 end
