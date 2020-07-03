24cdcf983b4c96f1ade7399ee222e19409d67ee1
Michael Evans <michaeljevans113@gmail.com>
2020-07-03 10:24:10 -0600

    Create test for listing pets at a specific shelter

---
+++ spec/features/shelters/pets_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Individual shelter's pets index page" do
+  it 'displays all (but only) pets available at that shelter' do
+    shelter_1 = Shelter.create!(name: 'Pets, Pets, Pets', address: '123 Easy St', city: 'Denver', state: 'CO', zip: '80204')
+    shelter_2 = Shelter.create!(name: 'Got A Lotta Pets', address: '456 EzPz Ln', city: 'LVegas', state: 'NV', zip: '12345')
+    pet_1 = shelter_1.pets.create!(name: 'Charles Barkley', age: 3, sex: 'Male', image: 'rhodesian.jpg', description: 'Rhodesian Ridgeback')
+    pet_2 = shelter_1.pets.create!(name: 'Karl Barx', age: 2, sex: 'Male', image: 'airedale.jpg', description: 'Airedale Terrier')
+    pet_3 = shelter_2.pets.create!(name: 'Sarah Jessica Barker', age: 4, sex: 'Female', image: 'dalmation.jpg', description: 'Dalmation')
+
+    visit "/shelters/#{shelter_1.id}/pets"
+
+    expect(page).to have_content("#{pet_1.name}")
+    expect(page).to have_content("Age: #{pet_1.age}")
+    expect(page).to have_content("Sex: #{pet_1.sex}")
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content("Age: #{pet_2.age}")
+    expect(page).to have_content("Sex: #{pet_2.sex}")
+    expect(page).to_not have_content("#{pet_3.name}")
+  end
+end
