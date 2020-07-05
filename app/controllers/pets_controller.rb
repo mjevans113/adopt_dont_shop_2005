14a3e97101e9c6573566b3c8c4944222cefc8b0b
Michael Evans <michaeljevans113@gmail.com>
2020-07-05 10:50:53 -0600

    Add #new and #create

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,18 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    Pet.create(pet_params)
+    redirect_to "/shelters/#{params[:shelter_id]}/pets"
+  end
+
+  private
+
+  def pet_params
+    params.permit(:name, :age, :sex, :description, :image, :status, :shelter_id)
+  end
 end
