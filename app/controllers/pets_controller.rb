c02ef2a13323bd734431fda6b18acec0ab0c31bf
Michael Evans <michaeljevans113@gmail.com>
2020-07-05 11:21:07 -0600

    Add #edit and #update

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,6 +15,16 @@ class PetsController < ApplicationController
     redirect_to "/shelters/#{params[:shelter_id]}/pets"
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+    redirect_to "/pets/#{@pet.id}"
+  end
+
   private
 
   def pet_params
